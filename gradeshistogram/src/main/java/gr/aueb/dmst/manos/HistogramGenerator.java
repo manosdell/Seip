17f3358c02168e3a2af1732334a8e9c72a74788c
Emmanouil Dellatolas <92676187+manosdell@users.noreply.github.com>
2023-04-21 11:12:13 +0300

    Change HistogramGenerator to create bar chart with grades' frequencies Issue: #5

--- gradeshistogram/src/main/java/gr/aueb/dmst/manos/HistogramGenerator.java
+++ gradeshistogram/src/main/java/gr/aueb/dmst/manos/HistogramGenerator.java
@@ -1,35 +1,43 @@
 package gr.aueb.dmst.manos;
 
 import java.io.*;
-import java.util.ArrayList;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
  * The HistogramGenerator class is responsible for reading the
  * grades from a txt file, given as a command line argument
- * and generating a histogram from them.
- * The grades are stored in an ArrayList<Float> and then
- * added to a dataset which is used to create the histogram.
+ * and generating a histogram (Bar chart) from them.
+ * The frequencies of the grades are stored in an array
+ * where the first element is the grade and the
+ * second element is the frequency of that grade.
  *
  * @author Emmanouil Dellatolas
- * @version 1.0
+ * @version 1.1
  * @since 2023-04-13
  */
 public class HistogramGenerator {
-     static ArrayList<Float> grades = new ArrayList<Float>();
+
+    static int[][] frequency = new int[11][2];
 
     /**
-     * The constructor of the HistogramGenerator class.
+     * The readFile() method reads the grades from the file
+     * and stores them in an array.
+     * The grades are stored in the first column of the array
+     * and the frequencies are stored in the second column.
      *
      * @param filename The name of the file that contains the grades.
      */
     public static void readFile(String filename) {
+
+        for (int i = 0; i < 11; i++) {
+            frequency[i][0] = i;
+            frequency[i][1] = 0;
+        }
 
         BufferedReader reader;
         try {
@@ -38,7 +46,11 @@ public class HistogramGenerator {
             // read the file line by line
             String currentLine = reader.readLine();
             while (currentLine != null) {
-                grades.add(Float.parseFloat(currentLine));
+                // add the grade to the ArrayList
+                int grade = Integer.parseInt(currentLine);
+                frequency[grade][1]++;
+
+                // read the next line
                 currentLine = reader.readLine();
 
             }
@@ -49,34 +61,44 @@ public class HistogramGenerator {
     }
 
     /**
-     * The generateHistogram() method creates a dataset from the
-     * grades and then creates a histogram from it.
+     * The generateHistogram() method creates a bar chart
+     * from the grades stored in the array. The grades are
+     * the categories of the chart and the frequencies are
+     * the values of the chart.
      */
     public static void generateHistogram() {
         // create series of data points from the file
-        XYSeries series = new XYSeries("Grades");
-        for (int i = 0; i < grades.size(); i++) {
-            series.add(i, grades.get(i));
+        String series1 = "Grade";
+        // create the dataset
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        for (int i = 0; i < 11; i++) {
+            dataset.addValue(frequency[i][1], series1, String.valueOf(i));
         }
-        XYSeriesCollection dataset = new XYSeriesCollection();
-        dataset.addSeries(series);
 
-        // create the XYLine chart
-        JFreeChart chart = ChartFactory.createXYLineChart("Grades", "Students", "Grades", dataset,
-                PlotOrientation.VERTICAL, true, true, false);
-        ChartFrame frame = new ChartFrame("Grades", chart);
+        // create the chart
+        JFreeChart chart = ChartFactory.createBarChart("Grades Histogram", // chart title
+                "Grade", // domain axis label
+                "Frequency", // range axis label
+                dataset, // data
+                PlotOrientation.VERTICAL, // orientation
+                true, // include legend
+                true, // tooltips
+                false // URLs?
+        );
+
+        // display the chart
+        ChartFrame frame = new ChartFrame("Grades Histogram", chart);
         frame.pack();
         frame.setVisible(true);
+
     }
 
     /**
      * The main method of the application.
      */
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         readFile(args[0]);
         generateHistogram();
     }
-
 
 }
